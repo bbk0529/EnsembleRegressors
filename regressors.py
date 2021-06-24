28b7e9912218278af73fec3fb2ecf2a0325f7573
Byoungkyu Park <byoungkyu.park@kisters.de>
2021-06-24 18:19:01 +0200

    updated with some changes

--- regressors.py
+++ regressors.py
@@ -60,16 +60,21 @@ class SpatialComparision(Classifier) :
 
 
 class EnsembleRegression(Classifier) :
-    def __init__(self, n_regressors: int, n_variables: int, eps: float, decision_boundary: float) :
+    def __init__(self, n_regressors: int, n_variables: int, eps: float, decision_boundary: float, global_search = False) :
         self._n_regressors = n_regressors
         self._n_variables = n_variables
         self._eps = eps
         self._decision_boundary = decision_boundary
+        self._global_search = global_search
 
     def validate(self, ts_data, neighbor, station) :
         result = np.array([], dtype='int')
         for i in range(self._n_regressors) : 
-            idx = np.random.choice(neighbor[station], size=self._n_variables, replace=False)
+            if self._global_search : 
+                idx = np.random.choice(range(len(ts_data)), size=self._n_variables, replace=False)
+            else : 
+                idx = np.random.choice(neighbor[station], size=self._n_variables, replace=False)
+            
             y = ts_data[station] 
             X = ts_data[idx]
             reg = LinearRegression().fit(X.T, y)
@@ -85,6 +90,29 @@ class EnsembleRegression(Classifier) :
         # print(RESULT)
         # return [str(station) + '_' + str(k) for k, v in sorted(RESULT.items(), reverse=True, key=lambda item: item[1]) if v>(n_regressors/3*2)]
         return [str(station) + '_' + str(k) for k, v in RESULT.items() if v>(self._n_regressors * self._decision_boundary)]
+
+    def validate_and_predict(self, ts_data, neighbor, station):    
+        width = ts_data.shape[1]
+        PRED = np.zeros((self._n_regressors, width))
+        SCORE = np.zeros(self._n_regressors)
+        for j in range(self._n_regressors) :             
+            idx_variables = np.random.choice(range(self._n_variables), replace=False, size=self._n_variables)
+            y = ts_data[station]
+            X = ts_data[neighbor[station]][idx_variables]
+            reg = LinearRegression().fit(X.T,y)
+            score = reg.score(X.T, y)
+            pred = reg.predict(ts_data[neighbor[station]][idx_variables].T)            
+            PRED[j] = pred
+            SCORE[j] = score
+        SCORE = SCORE / np.sum(SCORE)
+        predict = np.dot(PRED.T, SCORE)
+        original = ts_data[station]
+        ix = np.where(abs(predict - original) > self._eps)
+        if len(ix[0]) <= 0 : 
+            return None
+        else : 
+            return [ix, pred[ix]]
+    
 
 class EnsembleRegression2(Classifier) :
     def __init__(self, n_regressors: int, n_variables: int, eps: float, decision_boundary: float) :
@@ -139,18 +167,40 @@ class EnsembleRegressionBootstrap(Classifier) :
         ix = np.where(abs(predict - original) > self._eps)    
         return [str(station) + '_' + str(idx) for idx in ix[0]]
 
+class EnsembleRegressionCorrector(Classifier) :
+    def __init__(self, n_regressors: int, n_variables: int, eps: float) :
+        self._n_regressors = n_regressors
+        self._n_variables = n_variables
+        self._eps = eps        
 
+    def validate(self, ts_data, neighbor, station) :
+        width = ts_data.shape[1]
+        PRED = np.zeros((self._n_regressors, width))
+        SCORE = np.zeros(self._n_regressors)
+        for j in range(self._n_regressors) : 
+            idx_samples = np.unique(np.random.choice(range(len(ts_data[station])), replace=True, size=40))
+            idx_variables = np.random.choice(range(self._n_variables), replace=False, size=self._n_variables)
+            y = ts_data[station][idx_samples]
+            X = ts_data[neighbor[station]][idx_variables][:, idx_samples]
+            reg = LinearRegression().fit(X.T,y)
+            score = reg.score(X.T, y)
+            pred = reg.predict(ts_data[neighbor[station]][idx_variables].T)            
+            PRED[j] = pred
+            SCORE[j] = score
+        SCORE = SCORE / np.sum(SCORE)
+        predict = np.dot(PRED.T, SCORE)
+        original = ts_data[station]
+        ix = np.where(abs(predict - original) > self._eps)    
+        ts_data[station, ix] = predict[ix]
+        return [str(station) + '_' + str(idx) for idx in ix[0]]
 
 class Data(ABC):
-    def __init__(self, p_noise):
-        self._create_neighor_list(self._metadata[:, 1:], self._k)       
-        self._flag_noise = False 
-        if p_noise :             
-            ts_rawdata = copy.deepcopy(self._ts_rawdata)
-            # self.ts_data, self.lst_station_timestep, self.lst_noises = self.add_noise(ts_rawdata, p_noise=p_noise, min_noise=1, max_noise=10)
-            self.ts_data, self.lst_station_timestep = self.add_noise3(ts_rawdata, p_noise=p_noise)
-        else : 
-            self.ts_data = self._ts_rawdata
+    def __init__(self, p_noise_stations, p_noise_timesteps):
+        self._create_neighor_list(self._metadata[:, 1:], self._k)                        
+        ts_rawdata = copy.deepcopy(self._ts_rawdata)        
+        self.ts_data, self.lst_station_timestep = self.add_noise3(ts_rawdata, p_noise_stations=p_noise_stations, p_noise_timesteps = p_noise_timesteps)
+        
+        
         
 
     def _create_neighor_list(self, metadata: np.ndarray, k: int): 
@@ -174,18 +224,22 @@ class Data(ABC):
         lst_noises ={}
         return ts_data + matrix_noises, [str(x[0]) + '_' + str(x[1]) for x in np.array(idx).T]
 
-    def add_noise3(self, ts_data, p_noise: float) :
+    def add_noise3(self, ts_data, p_noise_stations: float, p_noise_timesteps: float) :
         n_stations = ts_data.shape[0]
         n_timesteps = ts_data.shape[1]
-
+        self.dic_timesteps ={}
         matrix_noises = np.zeros(ts_data.shape)
-        pick_stations = np.random.choice(range(n_stations), size= round(p_noise * n_stations))
-        for s in pick_stations : 
-            pick_timesteps = np.random.choice(range(n_timesteps), size= round(0.05 * n_timesteps), replace=False)
-            matrix_noises[s, pick_timesteps ] = np.random.choice(np.arange(3,5,1)) * np.random.choice([-1,1]) - np.random.randn(len(pick_timesteps)) * 10 
+        picked_stations = np.random.choice(range(n_stations), size= round(p_noise_stations * n_stations))
+        picked_stations.sort()
+        self._picked_stations = picked_stations
+        for s in picked_stations : 
+            pick_timesteps = np.random.choice(range(n_timesteps), size= round(p_noise_timesteps * n_timesteps), replace=False)
+            matrix_noises[s, pick_timesteps ] = np.random.rand(len(pick_timesteps)) * 10 
+            self.dic_timesteps[s] = pick_timesteps
+            # matrix_noises[s, pick_timesteps ] = np.random.choice(np.arange(3,5,1)) * np.random.choice([-1,1]) - np.random.randn(len(pick_timesteps)) * 10 
         idx = np.where(abs(matrix_noises)>0)
         lst_noises ={}
-        print(pick_stations)
+        self.matrix_noises = matrix_noises
         return ts_data + matrix_noises, [str(x[0]) + '_' + str(x[1]) for x in np.array(idx).T]
 
 
@@ -215,13 +269,13 @@ class Data(ABC):
 
 
 class Tempearture_DWD(Data) : 
-    def __init__(self, n_stations: int, n_timesteps: int, k: int=5, p_noise: float=None) : 
+    def __init__(self, n_stations: int, n_timesteps: int, k: int=5, p_noise_stations: float=0.1, p_noise_timesteps: float=0.1) : 
         df = pickle.load(open('data_dvd_reduced.p','rb'))
         df_metadata = pickle.load(open('metadata.p', 'rb'))            
         self._metadata = df_metadata.values[:n_stations]        
         self._ts_rawdata = self._preprocess_data(df.values[:n_stations, :n_timesteps]   )
         self._k = k          
-        super().__init__(p_noise)
+        super().__init__(p_noise_stations, p_noise_timesteps)
 
     def _preprocess_data(self, ts_data: np.ndarray) : 
         df = pd.DataFrame(ts_data)
@@ -264,7 +318,7 @@ class RandomData(Data) :
         return ts_data        
 
 
-class Validator() : 
+class Executor() : 
     def __init__ (self, data: Data, classifier: Classifier) : 
         self._data = data
         self._classifier = classifier
@@ -325,7 +379,39 @@ class Validator() :
             }
         )
 
+    def _regression(X,y) : 
+        reg = LinearRegression().fit(X.T,y)
+        score = reg.score(X.T,y)
+        pred = reg.predict(X.T)
+        # print(score)
+        # plt.plot(pred,color="red")
+        # plt.plot(y)
+        # plt.figure()
+        # plt.plot(gap)
+        return pred
 
+    def _correct_values(pred, y): 
+        gap = abs(pred - y)
+        idx_for_correction = np.where(gap > np.mean(gap) + 2 * np.std(gap))[0]
+        corrected_y = copy.deepcopy(y)
+        if len(idx_for_correction) < 1 : 
+            print("no more correction possible")
+            return y
+        else : 
+            print("idx for correction", idx_for_correction)
+        for i in idx_for_correction : 
+            corrected_y[i] = pred[i]
+        return corrected_y
+
+
+    def correct_values(self) :         
+        for i in range(len(self._data.ts_data)) :
+            pred = self.validate_and_predict(i)            
+            
+            pred = regression(X,y)
+            corrected_y = correct_values(pred, y)
+            ts_data[i] = corrected_y
+            
     
 
 
@@ -350,9 +436,10 @@ class Validator() :
         STAT['n_tsdata'] = self._data.ts_data.size
         STAT['p_noises'] = len(self._data.lst_station_timestep) / self._data.ts_data.size 
         STAT['TN'] = self._data.ts_data.size - STAT['TP'] - STAT['FP'] - STAT['FN']
-        STAT['precision'] = STAT['TP'] / (STAT['TP'] + STAT['FP'])
-        STAT['recall'] = STAT['TP'] / (STAT['TP'] + STAT['FN'])
-        STAT['f1'] = 2 * STAT['precision'] * STAT['recall'] / (STAT['precision'] + STAT['recall'])
+        STAT['precision'] = round(STAT['TP'] / (STAT['TP'] + STAT['FP']),3)
+        STAT['recall'] = round(STAT['TP'] / (STAT['TP'] + STAT['FN']),3)
+        STAT['f1'] = round(2 * STAT['precision'] * STAT['recall'] / (STAT['precision'] + STAT['recall']),3)
+        self.result = RESULT        
         return STAT
 
 
@@ -421,18 +508,64 @@ def regression_based_outlier_detection3(ts_data, neighbor, station, n_regressors
     
     
 if __name__== '__main__' :
-    n_stations = 100
+    n_stations = 450
     n_timesteps = 100
-    k=5
-    data = Tempearture_DWD(n_stations, n_timesteps, k)
-    classifier = EnsembleRegression()
-    classifier = SpatialComparision()
-    validator = Validator(data, classifier, 7,5,3)
-    s = random.choice(list(set([k[0] for k,v in data.lst_noises.items()])))
+    k = 5
+    p_noise_stations = 0.1
+    p_noise_timesteps = 0.20
+
+    data = Tempearture_DWD(n_stations, n_timesteps, k, p_noise_stations=p_noise_stations, p_noise_timesteps= p_noise_timesteps)
+    ensembleregression = EnsembleRegression(n_regressors=5,n_variables=3, eps=2, decision_boundary=0.6, global_search=True)
+
+    executor = Executor(data, ensembleregression)
+    print(data._picked_stations)
+    # station = np.random.choice(data._picked_stations)
+    toggle = {}
+    total_sum = 0 
+    for s in data._picked_stations :                 
+        dic = {}
+        for t in data.dic_timesteps[s] : 
+            dic[t] = False 
+            total_sum += 1
+        toggle[s] = dic
     
-    result = validator.validate(s)
-    print(s, result)
+    eps = 5
+    lst_false_positive =[]
+    while (True) : 
+        ensembleregression = EnsembleRegression(n_regressors=5,n_variables=3, eps=eps, decision_boundary=0.7, global_search=True)
+        executor = Executor(data, ensembleregression)
+        old_SSE = math.sqrt(np.sum((data.ts_data - data._ts_rawdata) ** 2))
+        print("with eps: ", eps)
+        print("before correction", old_SSE)
+        
+        
+        for station in range(n_stations) : 
+            result = executor._classifier.validate_and_predict(data.ts_data, data._neighbor, station)            
+            if result :                 
+                
+                if toggle.get(station) != None : 
+                    for r in result[0][0] : 
+                        toggle[station][r] = True 
+                else : 
+                    print(station, "is false positive")        
+                    lst_false_positive.append([station,result[0]])
+                #update
+                data.ts_data[station,result[0]] = result[1]
+        
+        new_SSE = math.sqrt(np.sum((data.ts_data - data._ts_rawdata) ** 2))
+        print("after correction", new_SSE)        
+        
+        if (abs(old_SSE - new_SSE) / old_SSE) < 0.1 : 
+            
+            break    
+    print("false positive", list(lst_false_positive))
+    s = 0
+    for t in toggle : 
+        s += sum(toggle[t].values())
+    print(s, total_sum)
+    print("false positive", lst_false_positive)
     
+
     
 
 
